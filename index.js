c48efe3a3bb1d783d83f2492131bc30f144b50fb
Bruno Possiedi <brunooaps@gmail.com>
2023-06-20 15:49:31 -0300

    initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const axios = require('axios')
+const exphbs = require('express-handlebars')
+
+const app = express()
+
+const hbs = exphbs.create({defaultLayout: 'main'})
+
+app.engine('handlebars', hbs.engine)
+
+app.set('/pokemon/:name', async (req, res) => {
+    try {
+        const {name} = req.params
+        const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${name}`)
+        const pokemon = response.defaultLayout
+    
+        res.render('pokemon', pokemon)
+    } catch (error) {
+        res.send(404).render('error', {message: error})
+    }
+})
+
+const PORT = 3030;
+app.listen(PORT, () => {console.log('Server is running in 3030')})
